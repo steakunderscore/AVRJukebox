544cc0948d96d4c10e017aacecf026778ab3c951
Wim Looman <ghostunderscore@gmail.com>
2010-04-16 16:07:50 +1200

    Cleaned up main file

--- main.c
+++ main.c
@@ -1,19 +1,12 @@
-//*----------------------------------------------------------------------------
-//*         ATMEL Microcontroller Software Support  -  ROUSSET  -
-//*----------------------------------------------------------------------------
-//* The software is delivered "AS IS" without warranty or condition of any
-//* kind, either express, implied or statutory. This includes without
-//* limitation any warranty or condition with respect to merchantability or
-//* fitness for any particular purpose, or against the infringements of
-//* intellectual property rights of others.
-//*----------------------------------------------------------------------------
-//* File Name: 		main.c
-//* Object: 		main application written in C
-//* Creation: 		JPP   16/Jun/2004
-//* Modifications:	Steve Weddell for ENEL353/COSC361, March, 2008
-//*                 Minor mods, Feb 22, 2010
-//* Purpose:		Produces a "Knight-Rider" effect on a bank of 8-LEDs
-//*----------------------------------------------------------------------------
+/*
+ * Authors
+ *      Wim Looman
+ *      Henry Jenkins
+ * Copyright
+ *      Copyright (c) 2010 Wim Looman, Henry Jenkins
+ * License
+ *      GNU General Public License (see LICENSE in root folder)
+ */
 
 // Include Standard files
 #include "Board.h"
@@ -22,45 +15,7 @@
 #include "sound.h"
 
 /* Global variables */
-#define SPEED 		(MCKKHz/20)
-unsigned int LedSpeed = SPEED *50 ;
-
-//Function Prototypes
-static void change_speed ( void );
-static void wait ( void );
-
-
-//*--------------------------------------------------------------------------------------
-//* Function Name       : change_speed
-//* Object              : Adjust "LedSpeed" value depending on SW1 and SW3 are pressed or not
-//* Input Parameters    : none
-//* Output Parameters   : Update of LedSpeed value.
-//*--------------------------------------------------------------------------------------
-static void change_speed ( void )
-{//* Begin
-    if ( (AT91F_PIO_GetInput(AT91C_BASE_PIOA) & SW1) == 0 )
-    {
-        if ( LedSpeed > SPEED ) LedSpeed -=SPEED ;
-    }
-    if ( (AT91F_PIO_GetInput(AT91C_BASE_PIOA) & SW3) == 0 )
-    {
-        if ( LedSpeed < MCK ) LedSpeed +=SPEED ;
-    }
-}//* End
-
-//*--------------------------------------------------------------------------------------
-//* Function Name       : wait
-//* Object              : Software waiting loop
-//* Input Parameters    : none. Waiting time is defined by the global variable LedSpeed.
-//* Output Parameters   : none
-//*--------------------------------------------------------------------------------------
-static void wait ( void )
-{//* Begin
-    volatile unsigned int waiting_time ;
-    change_speed () ;
-    for(waiting_time = 0; waiting_time < LedSpeed; waiting_time++) ;
-}//* End
-
+#define SPEED (MCKKHz/20)
 
 //*--------------------------------------------------------------------------------------
 //* Function Name       : Main
@@ -69,12 +24,11 @@ static void wait ( void )
 //* Output Parameters   : none.
 //*--------------------------------------------------------------------------------------
 int main(void)
-{//* Begin
+{
+    displayInit();
+    keypadInit();
 
-        keypadInit();
-
-       for(;;) {
-           scrollDisplay();
-       }
-}//* End of main
-
+    for(;;) {
+        scrollDisplay();
+    }
+}
