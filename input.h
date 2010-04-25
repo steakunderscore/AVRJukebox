743f4ef128079d092df7f02f6c389cedb35b1770
Nemo157 <nemo157@lol.(none)>
2010-04-26 10:30:43 +1200

    Updated the pin outs for the keypad

--- input.h
+++ input.h
@@ -15,14 +15,14 @@
 
 
 // Keypad Pin Definitions
-#define K_ROW_0     (1<<7)   // PA7
-#define K_ROW_1     (1<<8)   // PA8
-#define K_ROW_2     (1<<9)   // PA9
-#define K_ROW_3     (1<<10)  // PA10
-#define K_COLUMN_0  (1<<11)  // PA11
-#define K_COLUMN_1  (1<<12)  // PA12
-#define K_COLUMN_2  (1<<13)  // PA13
-#define K_COLUMN_3  (1<<14)  // PA14
+#define K_ROW_0    (1<<1) // PA1
+#define K_ROW_1    (1<<2) // PA2
+#define K_ROW_2    (1<<3) // PA3
+#define K_ROW_3    (1<<4) // PA4
+#define K_COLUMN_0 (1<<5) // PA5
+#define K_COLUMN_1 (1<<6) // PA6
+#define K_COLUMN_2 (1<<7) // PA7
+#define K_COLUMN_3 (1<<8) // PA8
 
 #define K_ROWS      (K_ROW_0 | K_ROW_1 | K_ROW_2 | K_ROW_3)
 #define K_COLUMNS   (K_COLUMN_0 | K_COLUMN_1 | K_COLUMN_2 | K_COLUMN_3)
