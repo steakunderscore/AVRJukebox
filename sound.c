f7a6c2d7c59bf4f42492e0671fcb87a20ac13ff2
Nemo157 <nemo157@lol.(none)>
2010-04-27 13:40:10 +1200

    Updated the sound file.

--- sound.c
+++ sound.c
@@ -11,5 +11,12 @@
 #include "sound.h"
 
 void soundInit( void ) {
+    AT91F_SPI_Enable(AT91C_BASE_SPI)
     //TODO: initilize the output for the DAC
+}
+
+void sendData( int data ) {
+    int control = 0x9;
+    AT91F_SPI_CfgCs(AT91C_BASE_SPI, 0, 1)
+    AT91F_SPI_SendFrame(AT91C_BASE_SPI
 }
