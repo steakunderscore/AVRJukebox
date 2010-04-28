4aba45ee004359a0e13851a668f4d141c9d0ae4c
Henry Jenkins <henry@henryjenkins.name>
2010-04-28 20:25:47 +1200

    Fixed the displayed charracters Added the config semgent io code Rearanged code for scrollDisplay to stop ghosting Cleaned scrollDisplay method Fixed bug with setDisplay method

--- display.c
+++ display.c
@@ -13,16 +13,25 @@
 const int display_mask[4]= {DSTATE0, DSTATE1, DSTATE2, DSTATE3};
 static uint8_t currentSegment = 0;
 static uint8_t display[] = {0, 0, 0, 0};
-const uint8_t states[] = { 0x03, 0x60, 0x6D, 0x79, 0x66,
-                           0xB6, 0xB6, 0xE0, 0xFC, 0xE4 };
+const uint8_t states[] = { 0x00, 0x60, 0xDA, 0xF2, 0x66,
+                           0xB6, 0xBE, 0xE0, 0xFE, 0xE6 };
 
 void displayInit() {
-    AT91F_PIO_CfgOutput( AT91C_BASE_PIOA, DISPLAY_MASK ) ;
-    AT91F_PIO_SetOutput( AT91C_BASE_PIOA, DISPLAY_MASK) ;
+    //Setup the display diget selecting outputs
+    AT91F_PIO_CfgOutput( AT91C_BASE_PIOA, DISPLAY_MASK );
+    AT91F_PIO_SetOutput( AT91C_BASE_PIOA, DISPLAY_MASK);
+    //Setup the segment outputs
+    AT91F_PIO_CfgOutput( AT91C_BASE_PIOA, DISPLAY_SEGMENT_MASK );
+    AT91F_PIO_SetOutput( AT91C_BASE_PIOA, DISPLAY_SEGMENT_MASK );
+
 }
 
 void scrollDisplay( void ) {
-    if (currentSegment >= 3) { //This could be changed to ==3
+    // Clear the display segments
+    AT91F_PIO_SetOutput( AT91C_BASE_PIOA, DISPLAY_SEGMENT_MASK);
+
+    // Scroll the display diget
+    if (currentSegment >= 3) {
         AT91F_PIO_SetOutput( AT91C_BASE_PIOA, DISPLAY_MASK) ;
         AT91F_PIO_ClearOutput( AT91C_BASE_PIOA, display_mask[0] );
         currentSegment = 0;
@@ -32,43 +41,38 @@ void scrollDisplay( void ) {
         AT91F_PIO_ClearOutput( AT91C_BASE_PIOA, display_mask[currentSegment]);
     }
 
-    /* First set all the segments to off, then turn on the ones we want on*/
-    AT91F_PIO_SetOutput( AT91C_BASE_PIOA, DISPLAY_SEGMENT_MASK);
-    if ((display[currentSegment] & (1<<7)) == (1<<7)) { //Deal with seg a
+    if ((display[currentSegment] & (1<<7))) { //Deal with seg a
         AT91F_PIO_ClearOutput( AT91C_BASE_PIOA, DSEGMENTA);
     }
-    if ((display[currentSegment] & (1<<6)) == (1<<6)) {
+    if ((display[currentSegment] & (1<<6))) { // etc..
         AT91F_PIO_ClearOutput( AT91C_BASE_PIOA, DSEGMENTB);
     }
-    if ((display[currentSegment] & (1<<5)) == (1<<5)) {
+    if ((display[currentSegment] & (1<<5))) {
         AT91F_PIO_ClearOutput( AT91C_BASE_PIOA, DSEGMENTC);
     }
-    if ((display[currentSegment] & (1<<4)) == (1<<4)) {
+    if ((display[currentSegment] & (1<<4))) {
         AT91F_PIO_ClearOutput( AT91C_BASE_PIOA, DSEGMENTD);
     }
-    if ((display[currentSegment] & (1<<3)) == (1<<3)) {
+    if ((display[currentSegment] & (1<<3))) {
         AT91F_PIO_ClearOutput( AT91C_BASE_PIOA, DSEGMENTE);
     }
-    if ((display[currentSegment] & (1<<2)) == (1<<2)) {
+    if ((display[currentSegment] & (1<<2))) {
         AT91F_PIO_ClearOutput( AT91C_BASE_PIOA, DSEGMENTF);
     }
-    if ((display[currentSegment] & (1<<1)) == (1<<1)) {
+    if ((display[currentSegment] & (1<<1))) {
         AT91F_PIO_ClearOutput( AT91C_BASE_PIOA, DSEGMENTG);
     }
-    if ((display[currentSegment] & (1<<0)) == (1<<0)) {
+    if ((display[currentSegment] & (1<<0))) {
         AT91F_PIO_ClearOutput( AT91C_BASE_PIOA, DSEGMENTDP);
     }
-
-
-
 }
 
 void setDisplay(int displayNum, int value) {
     if (value > 9) {
         /* If the value if out of range, turn the display off */
-        display[displayNum] = 0x00;
+        display[displayNum] = states[0];
     }
     else {
-        display[displayNum] = states[value-1];
+        display[displayNum] = states[value];
     }
 }
