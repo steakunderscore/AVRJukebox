5dcb5f54e2652026dbe40a4bf603f24c07c64bc5
Henry Jenkins <henry@henryjenkins.name>
2010-04-29 22:15:33 +1200

    Testing code to show off the display working

--- main.c
+++ main.c
@@ -18,11 +18,26 @@
  */
 int main(void)
 {
+    int i = 0;
+    int j = 0;
     displayInit();
     keypadInit();
     soundInit();
 
     for(;;) {
+        i++;
+        if (i > 100066) {
+            i = 0;
+            setError(j++); 
+            /*setDisplay(0,j++);
+            setDisplay(1,j++);
+            setDisplay(2,j++);
+            setDisplay(3,j++);*/
+            if ( j > 0xF) {
+                j = 0;
+            }
+        }
+
         //int** inputs = getInputs();
         //processInputs(inputs);
         //...sound stuff
