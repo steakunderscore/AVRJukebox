6b4d4bc6a05001a98f1cf4df611f3191d9a4503e
Wim Looman <ghostunderscore@gmail.com>
2010-05-06 11:35:07 +1200

    Changed the sendData function to use uint8_t again and also to directly write the register instead of using the library function.

--- sound.c
+++ sound.c
@@ -9,28 +9,23 @@
  */
 
 #include "sound.h"
-#include <stdlib.h>
 
 #define SPI_PERIPH (AT91C_PA13_MOSI | AT91C_PA14_SPCK | AT91C_PA11_NPCS0 | AT91C_PA12_MISO)
-#define SPI_MODE ((AT91C_SPI_MSTR | AT91C_SPI_PS) & ~(AT91C_SPI_PCSDEC | AT91C_SPI_LLB))
+#define SPI_MODE ((AT91C_SPI_MSTR | AT91C_SPI_PS_FIXED) & ~(AT91C_SPI_PCSDEC | AT91C_SPI_LLB))
 #define CS_REG ((AT91C_SPI_NCPHA | AT91C_SPI_BITS_16 | (0x0A << 8)) & ~(AT91C_SPI_CSAAT | AT91C_SPI_CPOL))
 void soundInit( void ) {
-    AT91F_SPI_Enable(AT91C_BASE_SPI); // Enables the SPI.
     AT91F_PIO_CfgPeriph(AT91C_BASE_PIOA, SPI_PERIPH, 0x0);  //Sets the output lines required for the SPI.
     AT91F_SPI_CfgPMC(); // Enables the SPI periphial clock.
     AT91F_SPI_CfgMode (AT91C_BASE_SPI, SPI_MODE); //Sets the SPI's mode.
     AT91F_SPI_CfgCs(AT91C_BASE_SPI, 0, CS_REG); // Sets the clients settings.
+    AT91F_SPI_Enable(AT91C_BASE_SPI); // Enables the SPI.
 }
 
-void sendData( char data ) {
+void sendData( uint8_t data ) {
     while (! AT91F_PDC_IsTxEmpty((AT91PS_PDC) &(AT91C_BASE_SPI->SPI_RPR))) {
         // Wait
     }
     
-    static char output[2] = {0x9, data};
-    if (AT91F_SPI_SendFrame(AT91C_BASE_SPI, output, 16, 0x0, 0)) { //might need 2 instead of 16
-      //All's good
-    } else {
-      //PANIC
-    }
+    data = 0x09 << 8 + data
+    pSPI->SPI_TDR = data;
 }
