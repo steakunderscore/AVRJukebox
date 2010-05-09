e39f5374ad777bb8d2d2d66649e567c56ef8d5da
Henry Jenkins <henry@henryjenkins.name>
2010-05-10 10:13:17 +1200

    Fixed mistakes in files
    Fixed the io port numbers
    Removed the use of the precompiler macros
    debuged code

--- input.h
+++ input.h
@@ -16,14 +16,14 @@
 
 
 // Keypad Pin Definitions
-#define K_ROW_0    (1<<1) // PA1
-#define K_ROW_1    (1<<2) // PA2
-#define K_ROW_2    (1<<3) // PA3
-#define K_ROW_3    (1<<4) // PA4
-#define K_COLUMN_0 (1<<5) // PA5
-#define K_COLUMN_1 (1<<6) // PA6
-#define K_COLUMN_2 (1<<7) // PA7
-#define K_COLUMN_3 (1<<8) // PA8
+#define K_ROW_0    (1<<0) // PA0
+#define K_ROW_1    (1<<1) // PA1
+#define K_ROW_2    (1<<2) // PA2
+#define K_ROW_3    (1<<3) // PA3
+#define K_COLUMN_0 (1<<4) // PA4
+#define K_COLUMN_1 (1<<5) // PA5
+#define K_COLUMN_2 (1<<6) // PA6
+#define K_COLUMN_3 (1<<7) // PA7
 
 #define K_ROWS      (K_ROW_0 | K_ROW_1 | K_ROW_2 | K_ROW_3)
 #define K_COLUMNS   (K_COLUMN_0 | K_COLUMN_1 | K_COLUMN_2 | K_COLUMN_3)
